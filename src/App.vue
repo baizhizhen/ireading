df82fa673e6515bda414e4812bd45a3ace5fb67d
baizhizhen <bzz_xd@sina.com>
2017-11-04 15:19:55 +0800

    init ireading project

---
+++ src/App.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="layout">
+    <Menu mode="horizontal" theme="primary" active-name="1">
+      <div class="layout-logo">
+        <Icon type="ios-lightbulb-outline" size="25"></Icon>
+        <span>IReading</span>
+      </div>
+      <div class="layout-nav">
+        <MenuItem name="1">
+          <Icon type="compose"></Icon>
+          基础数据
+        </MenuItem>
+        <MenuItem name="2">
+          <Icon type="flag"></Icon>
+          计划
+        </MenuItem>
+        <MenuItem name="3">
+          <Icon type="compass"></Icon>
+          跟踪
+        </MenuItem>
+        <MenuItem name="4">
+          <Icon type="stats-bars"></Icon>
+          统计
+        </MenuItem>
+      </div>
+    </Menu>
+    <div class="layout-content">
+      <Row>
+        <Col :span="5" v-show="isShowLeftNavBar">
+        <Menu active-name="1-2" width="auto" :open-names="['1']">
+          <Submenu name="1">
+            <template slot="title">
+              <Icon type="ios-navigate" :size="14"></Icon>
+              Item 1
+            </template>
+            <MenuItem name="1-1">Option 1</MenuItem>
+            <MenuItem name="1-2">Option 2</MenuItem>
+            <MenuItem name="1-3">Option 3</MenuItem>
+          </Submenu>
+          <Submenu name="2">
+            <template slot="title">
+              <Icon type="ios-keypad" :size="14"></Icon>
+              Item 2
+            </template>
+            <MenuItem name="2-1">Option 1</MenuItem>
+            <MenuItem name="2-2">Option 2</MenuItem>
+          </Submenu>
+          <Submenu name="3">
+            <template slot="title">
+              <Icon type="ios-analytics" :size="14"></Icon>
+              Item 3
+            </template>
+            <MenuItem name="3-1">Option 1</MenuItem>
+            <MenuItem name="3-2">Option 2</MenuItem>
+          </Submenu>
+        </Menu>
+        </Col>
+        <Col :span="spanRight">
+        <div class="layout-breadcrumb">
+          <Button type="text" @click="toggleClick">
+            <Icon type="navicon" size="25"></Icon>
+          </Button>
+          <Breadcrumb>
+            <BreadcrumbItem href="#">Home</BreadcrumbItem>
+            <BreadcrumbItem href="#">Projects</BreadcrumbItem>
+            <BreadcrumbItem>iView</BreadcrumbItem>
+          </Breadcrumb>
+        </div>
+        <div class="layout-content-main">Content</div>
+        </Col>
+      </Row>
+    </div>
+    <div class="layout-copy">
+      2011-2016 &copy; IReading
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        isShowLeftNavBar: true
+      }
+    },
+    computed: {
+      spanRight () {
+        return this.isShowLeftNavBar ? 19 : 24
+      }
+    },
+    methods: {
+      toggleClick () {
+        this.isShowLeftNavBar = !this.isShowLeftNavBar
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  @import "assets/sass/index";
+
+  $copy-right-text-color: #9ea7b4;
+  $full-height: 100%;
+  $page-background-color: #f5f7f9;
+  $content-background-color: #fff;
+  .layout {
+    border: 1px solid #d7dde4;
+    background: $page-background-color;
+    position: relative;
+    height: $full-height;
+    width: 100%;
+    .layout-breadcrumb {
+      padding: 10px 15px 0;
+      & > * {
+        display: inline-block;
+      }
+    }
+
+    .layout-logo {
+      border-radius: 3px;
+      float: left;
+      margin-left: 20px;
+      span {
+        font-size: 16px;
+        color: #fff;
+      }
+      .ivu-icon{
+        color: #fff;
+      }
+    }
+
+    .layout-nav {
+      width: 420px;
+      margin: 0 auto;
+    }
+
+    .layout-breadcrumb {
+      padding: 10px 15px 0;
+    }
+
+    .layout-content {
+      min-height: 200px;
+      margin: 15px;
+      overflow: hidden;
+      height: calc(100% - 138px);
+      background: $content-background-color;
+      border-radius: 4px;
+      .ivu-row, .ivu-col, .ivu-menu {
+        height: $full-height;
+      }
+    }
+
+    .layout-content-main {
+      padding: 10px;
+    }
+
+    .layout-copy {
+      text-align: center;
+      padding: 10px 0 20px;
+      color: $copy-right-text-color;
+    }
+  }
+</style>
